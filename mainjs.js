4d0f6ebf509b70f3c2c08a851b2b1a5657d29c5c
oAmorim2k5 <victorhuski@gmail.com>
2024-07-03 18:48:56 -0300

    segundo commit - js

--- mainjs.js
+++ mainjs.js
@@ -1,14 +1,25 @@
-class pessoa{
-    nome
-    idade
-    peso
-    altura
+
+class pessoa
+{   
+    //Variáveis tipo caracteristicas
+    _nome    //tipo string
+    _idade   //tipo int
+    _peso    //tipo float
+    _altura  //tipo float
+    _imc //tipo float
+
+    //Metodos
+    constructor(nome, idade, peso, altura ){
+        this._nome = nome
+        this._idade = idade
+        this._peso = peso
+        this._altura = altura
+    }
 }
 
-let pessoa1 = new pessoa()
-pessoa1.nome = "Luana"
-pessoa1.idade = 
-pessoa1.peso = 
-pessoa1.altura = 
+//Instanciando novo objeto a partir da classe pessoa
+let pessoa1 = new pessoa("Victor", 18, 63, 1.80)
+let pessoa2 = new pessoa("Luan", 1, 55, 1.67)
 
 console.log(pessoa1)
+console.log(pessoa2)

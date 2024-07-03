02a16403e576f8142f2b4efa67095da5c91d7806
oAmorim2k5 <victorhuski@gmail.com>
2024-07-03 18:13:27 -0300

    Aplicando conhecimento em POO - simples

---
+++ mainjs.js
@@ -0,0 +1,14 @@
+class pessoa{
+    nome
+    idade
+    peso
+    altura
+}
+
+let pessoa1 = new pessoa()
+pessoa1.nome = "Luana"
+pessoa1.idade = 
+pessoa1.peso = 
+pessoa1.altura = 
+
+console.log(pessoa1)
